510c8a042f57d28a47b02ceff16230ad2bfc80c6
vinay pancherpula <vinayvarmap1999@gmail.com>
2023-06-06 21:52:20 -0700

    hey

---
+++ helll/src/helll/virat.java
@@ -0,0 +1,17 @@
+package helll;
+
+public class virat {
+	
+	
+	public void hi() {
+		System.out.println("hi all");
+	}
+	public void hello() {
+		System.out.println("hello all");
+	}
+public static void main(String[] args) {
+	virat k = new virat();
+	k.hi();
+	k.hello();
+}
+}
